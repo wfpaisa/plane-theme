ba15c8b1ff33f4fa39a476e665921d83c55bd4a9
wfpaisa <wfpaisa@gmail.com>
2017-12-05 08:24:26 -0500

    gtk2,gtk3 ok

--- gulpfile.js
+++ gulpfile.js
@@ -20,6 +20,8 @@
  */
 
 "use strict";
+
+/* Dependencies */
 var fs = require('fs'),
 	path = require('path'),
 	gulp = require('gulp'),
@@ -30,58 +32,53 @@ var fs = require('fs'),
 	rename = require("gulp-rename");
 
 
+/* Global variables */
 var dirLight = 'Plane';
 var dirDark = 'Plane-dark';
 
 
+/*========================================
+=            Global functions            =
+========================================*/
+
+
+
+/**
+ * Time Log + Message
+ *
+ * @param      {string}  m       message
+ */
 function timeLog(m){
 	var d = new Date();
 	var n = d.toTimeString().split(' ')[0];
 
-	console.log(`[${n}] Finished ${m}`);
+	console.log(`[${n}] ${m}`);
 }
 
-gulp.task('sass', function () {
 
-	return gulp.src("./src/gtk-3.0/**/*.scss")
-		.pipe(sass().on('error', sass.logError))
-		.pipe(rename(function (path) {
-
-			// Send dark files to dark directory
-			if(path.basename.indexOf('-dark') > -1 ){				
-				path.basename = path.basename.replace('-dark','');
-				path.dirname = `../../${dirDark}/gtk-3.0`;
-			}
-
-		}))
-		.pipe(gulp.dest(`./build/${dirLight}/gtk-3.0`)); 
-
-});
-
-
-// gulp.task('assets', function(){
-// 	return 	gulp.src("./src/gtk-3.0/assets/*")
-// 		.pipe(gulp.dest(`./build/${dirLight}/gtk-3.0/assets`))
-// 		.pipe(gulp.dest(`./build/${dirDark}/gtk-3.0/assets`));
-// })
-
+/**
+ * GTK Auto reload
+ */
 function autoreload(){
 
 	var theme = dirLight;
 
+	/* Active dark theme */
 	if (process.argv[process.argv.length - 1]) {
 
 		let argv = process.argv[process.argv.length - 1];
 		
 		if (argv == '-D') theme = dirDark;
 
-		// gsettings set  org.gnome.desktop.interface gtk-theme Adwaita
-		// gsettings set  org.gnome.desktop.interface gtk-theme Plane
+
+		/* First change default theme and late change to our theme */
 		exec(`gsettings set  org.gnome.desktop.interface gtk-theme Adwaita`, () => {
 			
 			exec(`gsettings set  org.gnome.desktop.interface gtk-theme ${theme}`, () => {
 				
 				timeLog(`Reload ${theme} theme`);
+
+				return;
 				
 			})			
 		})
@@ -90,32 +87,127 @@ function autoreload(){
 }
 
 
-gulp.task('watch', function () {
-  
-  gulp.watch('./src/gtk-3.0/**/*.scss', function(cb){
-
-
-  	/*
-  	 *	GTK3 and Autoreload
-  	 */
-	gulp.src("./src/gtk-3.0/**/*.scss")
+/**
+ * Sass 
+ * Generate css files
+ *
+ * @param      {string}    dir     The directory
+ * @param      {Function}  cb      callback
+ */
+function reSass(dir,cb){
+  	 
+	gulp.src(`./src/${dir}/**/*.scss`)
 			.pipe(sass().on('error', sass.logError))
 			.pipe(rename(function (path) {
 
 				// Send dark files to dark directory
 				if(path.basename.indexOf('-dark') > -1 ){				
 					path.basename = path.basename.replace('-dark','');
-					path.dirname = `../../${dirDark}/gtk-3.0`;
+					path.dirname = `../../${dirDark}/${dir}`;
 				}
 
 			}))
-			.pipe(gulp.dest(`./build/${dirLight}/gtk-3.0`))
-			.on('end',(cb)=>{
-				autoreload();
+			.pipe(gulp.dest(`./build/${dirLight}/${dir}`))
+			.on('end',()=>{
+				cb();
 			}); 
 
-  });  
+}
 
+/*=====  End of Global functions  ======*/
+
+
+
+/*====================================
+=            Common tasks            =
+====================================*/
+
+/*----------  gtk-3.0  ----------*/
+
+gulp.task('gtk3-sass', function (cb) {
+
+	reSass('gtk-3.0', function(){
+		cb();
+	}); 
+
+});
+
+
+gulp.task('gtk3-assets', function(){
+	return 	gulp.src("./src/gtk-3.0/assets/*")
+		.pipe(gulp.dest(`./build/${dirLight}/gtk-3.0/assets`))
+		.pipe(gulp.dest(`./build/${dirDark}/gtk-3.0/assets`));
+})
+
+
+/*----------  gtk-2.0  ----------*/
+
+gulp.task('gtk2-light', function(){
+	return 	gulp.src([
+						"./src/gtk-2.0/*",
+						"./src/gtk-2.0/*/*",
+						"!./src/gtk-2.0/*-dark",
+						"!./src/gtk-2.0/*-dark/*",
+					])
+				.pipe(gulp.dest(`./build/${dirLight}/gtk-2.0/`));
+		
+
+})
+
+gulp.task('gtk2-dark', function(){
+	return 	gulp.src([
+						"./src/gtk-2.0/*",
+						"./src/gtk-2.0/*/*",
+						"!./src/gtk-2.0/assets/*",
+					])
+			.pipe(rename(function (path) {
+
+				// Send dark files to dark directory
+				if(path.basename.indexOf('-dark') > -1 || path.dirname.indexOf('-dark') > -1 ){
+					path.basename = path.basename.replace('-dark','');
+					path.dirname = path.dirname.replace('-dark','');
+				}
+
+			}))
+			.pipe(gulp.dest(`./build/${dirDark}/gtk-2.0/`));
+})
+
+
+gulp.task('theme-light', function(){
+	return gulp.src('./src/index.theme')
+			.pipe(gulp.dest(`./build/${dirLight}`))
+})
+gulp.task('theme-dark', function(){
+	return gulp.src('./src/index-dark.theme')
+			.pipe(gulp.dest(`./build/${dirDark}/index.theme`))
+})
+/*=====  End of Common tasks  ======*/
+
+
+
+
+/*=============================
+=            Watch            =
+=============================*/
+
+gulp.task('watch', function () {
+  
+  // gulp.watch('./src/gtk-3.0/**/*.scss',  ['gtk3-sass'], function(cb){
+  // 		autoreload();
+  // 		cb()
+  // });  
+  gulp.watch('./src/gtk-3.0/**/*.scss',  function(cb){
+  		
+  		gulp.start('gtk3-sass', function(){
+  			autoreload();	
+  		})
+  		
+  });  
+  	
+
+
+  /* Images */
+  
   gulp.watch('./src/gtk-3.0/assets/*', function(file){
 
 	gulp.src(file.path)
@@ -132,12 +224,17 @@ gulp.task('watch', function () {
 
 }); 
 
-/*====================================
-=           .Render icons.           =
-====================================*/
-gulp.task('default', ['sass'], (cb) => {
-	// gulp.src('./src/**/*')
-	// 	.pipe(svg_icons_export())
-	// 	.pipe(gulp.dest(__dirname))
-	// 	.on('end', cb);
+/*=====  End of Watch  ======*/
+
+
+
+
+/**
+ *
+ * Generate Plane and Plane-dark theme
+ *
+ */
+
+gulp.task('default', ['theme-light','theme-dark','gtk3-sass', 'gtk3-assets', 'gtk2-light', 'gtk2-dark'], (cb) => {
+
 });
